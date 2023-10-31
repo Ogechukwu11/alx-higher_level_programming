11f8b6adbd26855d1fb3b1bc7f3ef1fd595984d6
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-10-31 22:00:19 +0000

    print lowercase alphabet without new line

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for character in range(97, 123):
-    print(chr(character), end="")#chr() is a function that coverts the ASCII value to character
+    print(chr(character), end="")
+    """chr() is a function that coverts the ASCII value to character"""
