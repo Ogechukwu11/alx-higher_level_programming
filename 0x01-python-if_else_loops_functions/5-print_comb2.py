b56134f5a4d84af84441dfbbfef78771d06f902e
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-10-31 22:46:53 +0000

    print numbers

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for num in range(0, 100):
+    if num < 10:
+        print("0{:d}".format(num), end=", " if num < 99 else "\n")
+    else:
+        print("{:d}".format(num), end=", " if num < 99 else "\n")
