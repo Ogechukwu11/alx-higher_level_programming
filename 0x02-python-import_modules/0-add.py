1cbb65793127a1915099faa987e3a9099a6efe34
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-11-02 12:16:31 +0000

    Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+# To ensure the code is not executed when imported
+if __name__ == "__main__":
+    pass
 # Import the add function from add_0.py
 from add_0 import add
 
@@ -7,6 +10,3 @@ b = 2
 # Calculate the result using the add function
 result = add(a, b)
 print("{:d}  + {:d} = {:d}".format(a, b, result))
-# To ensure the code is not executed when imported
-if __name__ == "__main__":
-    pass
