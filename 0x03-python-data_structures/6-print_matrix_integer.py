c380b0c6092cf6e12c613aa2516c074cc0913114
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-11-06 09:58:05 +0000

    Lists of lists = Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
         for row in matrix:
             for num in row:
                 print("{:d}".format(num), end=" " if num != row[-1] else "")
-                """end=" " if num != row[-1] else "": checks if 
-                the number is not the last one on the paper, we add a space; otherwise
-                we don't add anything after it."""
+                """end=" " if num != row[-1] else "": checks if
+                the number is not the last one on the paper, we add a space;
+                otherwise, we don't add anything after it."""
             print()
