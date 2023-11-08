d5c7b09b456586f1fa662a136ea0855f27aa99cc
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-11-08 17:57:29 +0000

    Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list:
-        add = 0
-        unique = set(my_list)
-        for num in unique:
-            add += num
-        return (add)
+    add = 0
+    unique = set(my_list)
+    for num in unique:
+        add += num
+    return (add)
