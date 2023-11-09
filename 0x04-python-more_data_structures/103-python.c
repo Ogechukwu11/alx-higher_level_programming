9a70ed1381ceded554b9fdb1233c94f98b7f3dc1
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-11-09 06:29:34 +0000

    CPython #1: PyBytesObject

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -5,7 +5,7 @@ void print_python_list(PyObject *p)
 {
 	if (PyList_Check(p))
 	{
-		Py_ssize_t size = PyList_Size(p);
+		Py_ssize_t size = ((PyVarObject *)p)->ob_size;
 		Py_ssize_t i;
 
 		printf("[*] Python list info\n");
@@ -14,7 +14,8 @@ void print_python_list(PyObject *p)
 
 		for (i = 0; i < size; i++)
 		{
-			printf("Element %zd: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+			PyObject *item = ((PyListObject *)p)->ob_item[i];
+			printf("Element %zd: %s\n", i, item->ob_type->tp_name);
 		}
 	}
 	else
@@ -27,17 +28,17 @@ void print_python_bytes(PyObject *p)
 {
 	if (PyBytes_Check(p))
 	{
-		Py_ssize_t size = PyBytes_Size(p);
+		Py_ssize_t size = ((PyVarObject *)p)->ob_size;
 		Py_ssize_t max_display = size < 10 ? size : 10;
 
 		printf("[.] bytes object info\n");
 		printf("size: %zd\n", size);
-		printf("trying string: %s\n", PyBytes_AsString(p));
+		printf("trying string: %s\n", PyUnicode_Check(p) ? "" : ((PyBytesObject *)p)->ob_sval);
 
 		printf("first %zd bytes: ", max_display);
 		for (Py_ssize_t i = 0; i < max_display; i++)
 		{
-			printf("%02x", (unsigned char)PyBytes_AS_STRING(p)[i]);
+			printf("%02x", (unsigned char)((PyBytesObject *)p)->ob_sval[i]);
 			if (i < max_display - 1)
 				printf(" ");
 		}
