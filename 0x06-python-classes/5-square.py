c3378f79ae39ce55dc832a32a642d5908c690ef2
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-12-31 08:51:51 +0000

    Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Defines a class Square """
+
+
+class Square:
+    """ Represents a Square """
+
+    def __init__(self, size=0):
+        """ Initialize the class attribute """
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """ To retrieve the private size attribute """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ To set the private size attribute to value """
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """ returns the current square area """
+        return self.size * self.size
+
+    def my_print(self):
+        """ prints in stdout the square with the character # """
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                print("#" * self.size)
