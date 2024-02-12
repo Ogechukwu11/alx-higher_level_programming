7ec7a68ce60a9372ddf22fac7660aa504e20b100
Ogechukwu11 <nwobumogechukwu@gmail.com>
2024-02-12 11:24:24 +0000

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const C = 'C is fun';
+const python = 'Python is cool';
+const javascript = 'JavaScript is amazing';
+
+console.log(C);
+console.log(python);
+console.log(javascript);
