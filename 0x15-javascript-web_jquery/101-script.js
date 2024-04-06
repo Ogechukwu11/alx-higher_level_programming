6d4b3f52c2911223a62e43c98e238f20b6678d53
Ogechukwu11 <nwobumogechukwu@gmail.com>
2024-04-06 08:52:57 +0000

    List, add, remove

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,10 +1,12 @@
 /* global $ */
-$('#add_item').click(function () {
-  $('.my_list').append('<li>Item</li>');
-});
-$('#remove_item').click(function () {
-  $('.my_list li:last').remove();
-});
-$('#clear_list').click(function () {
-  $('.my_list').empty();
+$(document).ready(function () {
+  $('#add_item').click(function () {
+    $('.my_list').append('<li>Item</li>');
+  });
+  $('#remove_item').click(function () {
+    $('.my_list li:last').remove();
+  });
+  $('#clear_list').click(function () {
+    $('.my_list').empty();
+  });
 });
